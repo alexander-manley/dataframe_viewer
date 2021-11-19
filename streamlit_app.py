0b49d6a35d4c2cfe60220cd51c53fcaaa8110d4a
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 18:57:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
-"""
-# Dataframe Viewer
-"""
+
 from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+
+"""
+# Dataframe Viewer
+"""
 
 url = 'https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv'
 df = pd.read_csv(url, index_col=0)
