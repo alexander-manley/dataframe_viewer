f969ba46d482c7385315eae8f55ed18cee5112f0
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 18:55:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 url = 'https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv'
 df = pd.read_csv(url, index_col=0)
-print(df.head(5))
+#print(df.head(5))
 
 df
