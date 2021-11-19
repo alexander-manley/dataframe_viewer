dbbab397eec92ece06a5f10dc551828ce4fc333c
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 18:49:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,11 @@ st.dataframe(df)  # Same as st.write(df)
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 """
 
-import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
+
+df
 
 st.dataframe(df)
