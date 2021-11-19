800a96350f498be17e2f24c301f4d331d03903e1
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 18:57:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,13 @@
+"""
+# Dataframe Viewer
+"""
 from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to the Dataframe Viewer App!!!!
-
-"""
-
-"""
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
-
-st.dataframe(df)  # Same as st.write(df)
-
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-"""
-
 url = 'https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv'
 df = pd.read_csv(url, index_col=0)
-#print(df.head(5))
 
 df
