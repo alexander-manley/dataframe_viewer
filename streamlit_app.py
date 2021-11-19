84772281015b285641771d5e810be8ffdbabb25e
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 18:47:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,19 @@ import streamlit as st
 # Welcome to the Dataframe Viewer App!!!!
 
 """
+
+"""
 df = pd.DataFrame(
    np.random.randn(50, 20),
    columns=('col %d' % i for i in range(20)))
 
 st.dataframe(df)  # Same as st.write(df)
+"""
+
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
