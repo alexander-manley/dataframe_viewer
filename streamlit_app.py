29bfd2e73d66ad1f9837724c48163c35f38a4635
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-11-19 20:30:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-
-#from collections import namedtuple
-#import altair as alt
-#import math
 import pandas as pd
-#import streamlit as st
+import streamlit as st
+import pycaret
 
 """
 # Dataframe Viewer
